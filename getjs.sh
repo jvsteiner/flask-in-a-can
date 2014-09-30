5011650b454a60494670e282d924e01c2c0c2e0c
Jamie Steiner <jvsteiner@gmail.com>
2014-09-30 09:38:39 +0300

    fixed shell script

--- getjs.sh
+++ getjs.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-wget -P static/ https://code.jquery.com/jquery-1.8.2.min.js --no-check-certificate
-wget -P static/ https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js --no-check-certificate
-wget -P static/ https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js --no-check-certificate
-wget -P static/ https://cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.4.1/knockout.mapping.min.js --no-check-certificate
-wget -P static/ https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css --no-check-certificate
-
+wget -P static/ https://code.jquery.com/jquery-1.8.2.min.js
+wget -P static/ https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js
+wget -P static/ https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js
+wget -P static/ https://cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.4.1/knockout.mapping.min.js
+wget -P static/ https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css
